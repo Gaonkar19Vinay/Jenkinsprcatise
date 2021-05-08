3ce5620d3eaccefc336db85ea71addb417febb4d
VINAY <VINAY@LAPTOP-BFM6S3H0>
2021-05-08 23:18:35 +0530

    My second commit

---
+++ com.crm.jnkn/src/test/java/com/crm/jnknpractise/Editproducttest.java
@@ -0,0 +1,10 @@
+package com.crm.jnknpractise;
+
+import org.testng.annotations.Test;
+
+public class Editproducttest {
+	@Test(groups="smoketest")
+	public void editproduct() {
+		System.out.println("editproduct");
+}
+}
---
+++ com.crm.jnkn/src/test/java/com/crm/jnknpractise/deleteprodocttest.java
@@ -0,0 +1,14 @@
+package com.crm.jnknpractise;
+
+import org.testng.annotations.Test;
+
+public class deleteprodocttest {
+  
+	
+	@Test(groups="smoketest")
+	public void deleteproduct() {
+		System.out.println("deletproduc");
+		
+		
+	}
+}
